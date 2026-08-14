20d72907176bf6fe6c1c1abc40174047b02c4546
voblya-dev <matvejolenuk71@gmail.com>
2026-08-14 17:56:18 +0800

    Release 1.1.1: refresh package URL and manifest

--- loader/__init__.py
+++ loader/__init__.py
@@ -2,6 +2,6 @@
 
 from __future__ import annotations
 
-LOADER_VERSION = "1.1.0"
+LOADER_VERSION = "1.1.1"
 
 __all__ = ["LOADER_VERSION"]
--- unlock/constants.py
+++ unlock/constants.py
@@ -7,7 +7,7 @@ import sys
 from pathlib import Path
 
 APP_NAME = "Unlock"
-APP_VERSION = "1.1.0"
+APP_VERSION = "1.1.1"
 
 # ---------------------------------------------------------------- paths
 
